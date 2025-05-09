8a5f801040e4b9e4c099298c703c4f159e139051
Alro John <alromercado08@gmail.com>
2025-05-09 09:47:56 +0800

    initial death report commit

---
+++ src/app/(dashboard)/reports-death/death-report-components/interface-death.tsx
@@ -0,0 +1,9 @@
+const DeathReportInterface = () => {
+  return (
+    <div>
+      <h1>DeathReportInterface</h1>
+    </div>
+  );
+};
+
+export default DeathReportInterface;
--- src/app/(dashboard)/reports-death/page.tsx
+++ src/app/(dashboard)/reports-death/page.tsx
@@ -1,7 +1,15 @@
+import { DashboardHeader } from "@/components/custom/dashboard/dashboard-header";
+
 const DeathReport = () => {
   return (
-    <div>
-      <h1>DeathReport</h1>
+    <div className="h-screen flex flex-col ">
+      <DashboardHeader
+        breadcrumbs={[
+          { label: "Dashboard", href: "/dashboard", active: false },
+          { label: "Reports", href: "/reports", active: true },
+        ]}
+      />
+      <div className="flex flex-1 flex-col gap-4 p-4 w-full overflow-y-auto h-full pb-4"></div>
     </div>
   );
 };
